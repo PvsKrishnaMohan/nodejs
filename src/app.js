ae001f8fbdf534175f04f71c73493b2be548916a
PvsKrishnaMohan <pvskrishnamohan479@gmail.com>
2024-12-11 16:45:24 +0530

    routes handles with authentication

--- src/app.js
+++ src/app.js
@@ -1,27 +1,24 @@
 const express = require('express');
 const app = express();
+const {adminAuth, userAuth} = require('./middlewares/adminAuth')
 
-app.use("/user",
+app.use("/admin", adminAuth);
+app.get("/admin",(req,res) => {
+    res.send("admin access !!!!!!!");
+});
 
-    (req,res,next) => {
-        console.log("1st handler request");
-        next();
-    },
-    (req,res,next) => {
-        console.log("2nd handler request");
-        next()
-    },
-    (req,res,next) => {
-        console.log("3rd req handler");
-        next()
-    },
-    (req,res)=>{
-        res.send("Hello from 4th req handler -user");
-        console.log("response sent!!!")
-    }
-)
-
-app.listen(5000,()=>{
-    console.log("server listening from port 5000 successfully!")
+app.get("/admin/data",adminAuth, (req,res) => {
+    res.send('Admin data sent succesfully!')
 })
 
+app.post("/adminLogin",(req,res) => {
+    res.send('admin data posted')
+})
+
+app.get("/admin/payroll",adminAuth,(req,res) => {
+    res.send("Admin payroll sent successfully!")
+})
+
+app.listen(5000,() => {
+    console.log("server listening from port 5000 successfully!")
+})
---
+++ src/middlewares/adminAuth.js
@@ -0,0 +1,28 @@
+const adminAuth = (req, res,next) => {
+    const auth = "abc123";
+    const isAuthorized = auth === "abc123o";
+
+    if(!isAuthorized) {
+        res.status(401).send("Access denied due to unAuthorization!")
+    }else{
+        console.log("Authentication is checking...");
+        next();
+    }
+}
+
+const userAuth = (req, res,next)=>{
+    const userAuth = '123abc';
+    const isAuthorized = auth === "123abc";
+
+    if(!isAuthorized){
+        res.status(401).status('User access is denied due to Authorization!')
+    }else {
+        console.log('User Authorized successfully!');
+        next();
+    }
+}
+
+module.exports = {
+    adminAuth,
+    userAuth
+}
